4ca15344741eb5803636daa886890015e110ae05
gitchan <eunkeeeee@gmail.com>
2024-03-28 15:29:41 +0900

    feat: change ui for nice system call

--- nice.c
+++ nice.c
@@ -4,19 +4,8 @@
 #include "fcntl.h"
 
 int
-main(int argc, char *argv[])
-{
-    int priority;
-    if(argc < 3){
-        printf(2,"Usage: nice pid priority\n");
-        exit();
-    }
-
-    priority = atoi(argv[2]);
-    if (priority < 0 || priority > 20){
-        printf(2,"Invalid priority (0-20)!\n");
-        exit();
-    }
+main(int argc, char *argv[]) {
+    int priority = atoi(argv[2]);
     chpr(priority);
     exit();
 }
