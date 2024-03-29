aafb2c9a48433d9c7259067dd55a9ef59231c0c5
gitchan <eunkeeeee@gmail.com>
2024-03-29 15:07:01 +0900

    test: yield system call test

---
+++ yieldtest.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void) {
+    int i, pid;
+    printf(1, "yield test\n");
+
+    pid = fork();
+    if (pid == 0) {
+        for ( i = 0; i < 10; ++i) {
+            printf(1, "child\n");
+            yield();
+        }
+    } else {
+        for (i = 0; i < 10; ++i) {
+            printf(1, " parent\n");
+            yield();
+        }
+    }
+    printf(1, "finished!\n");
+    exit();
+}
