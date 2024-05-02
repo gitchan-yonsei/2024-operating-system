8e8145f6acfff9e8e451d69645b1ab4f86eceab0
gitchan <eunkeeeee@gmail.com>
2024-05-02 14:45:21 +0900

    feat: add proc_priority property to proc struct

--- proc.h
+++ proc.h
@@ -52,6 +52,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  enum proc_priority priority;
 };
 
 // Process memory is laid out contiguously, low addresses first:
