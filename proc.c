474d15eb28319434879bffecaffb0d92e5047abb
gitchan <eunkeeeee@gmail.com>
2024-05-04 22:16:44 +0900

    refactor: wakeup1 개선

--- proc.c
+++ proc.c
@@ -519,13 +519,19 @@ static void
 wakeup1(void *chan)
 {
     struct proc *p;
+    int flag = 0;
+
     for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
         if (p->state == SLEEPING && p->chan == chan){
-            p->ticks = 0;
             p->state = RUNNABLE;
-
+            p->ticks = 0;
             enqueue(p);
+            flag = 1;
         }
+    }
+
+    if (flag) {
+        yield();
     }
 }
 
