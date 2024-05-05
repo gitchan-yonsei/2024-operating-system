3610402481d1ca21d675a556edd7271c3c5d3acd
gitchan <eunkeeeee@gmail.com>
2024-05-05 18:29:57 +0900

    fix: i 지우기

--- proc.c
+++ proc.c
@@ -525,7 +525,6 @@ static void
 wakeup1(void *chan)
 {
     struct proc *p;
-    int i;
 
     for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
         if (p->state == SLEEPING && p->chan == chan){
