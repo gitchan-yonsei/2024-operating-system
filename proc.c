4562924d7d6bf78b3c0deebcaeb71dcd0ae2ccb4
gitchan <eunkeeeee@gmail.com>
2024-05-05 18:29:38 +0900

    fix: clicks -> ticks

--- proc.c
+++ proc.c
@@ -529,7 +529,7 @@ wakeup1(void *chan)
 
     for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
         if (p->state == SLEEPING && p->chan == chan){
-            p->clicks = 0;
+            p->ticks = 0;
             p->state = RUNNABLE;
             enqueueFront(p);
 
