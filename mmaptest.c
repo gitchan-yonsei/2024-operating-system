61c6c1b31a0c6e8b9f7e45abedb8e0f289adf972
gitchan <eunkeeeee@gmail.com>
2024-05-13 18:04:55 +0900

    test: 테스트 코드 수정 22

--- mmaptest.c
+++ mmaptest.c
@@ -3,24 +3,27 @@
 #include "user.h"
 #include "fcntl.h"
 
-#define   MAP_PROT_READ 0x00000001
-#define   MAP_PROT_WRITE 0x00000002
+#define MAP_PROT_READ 0x00000001
+#define MAP_PROT_WRITE 0x00000002
 #define MAP_FAILED ((void*)-1)
 
 int main(int argc, char *argv[]) {
     int fd;
     char *memory;
 
+    // 파일 열기
     if ((fd = open("tempfile", O_CREATE | O_RDWR)) < 0) {
         printf(1, "Error opening file\n");
         exit();
     }
 
+    // 파일 크기를 4096 바이트로 확장
     int size = 4096;
-    char data = 0;
-    lseek(fd, size - 1, SEEK_SET);
-    write(fd, &data, 1);
+    char buffer[size];
+    memset(buffer, 0, size);
+    write(fd, buffer, size);
 
+    // mmap() 호출
     memory = mmap(fd, 0, size, MAP_PROT_READ | MAP_PROT_WRITE);
     if (memory == MAP_FAILED) {
         printf(1, "mmap failed\n");
@@ -28,14 +31,12 @@ int main(int argc, char *argv[]) {
         exit();
     }
 
+    // 메모리 매핑된 영역에 데이터 쓰기
     const char *msg = "Hello, mmap!";
     strcpy(memory, msg);
 
     // 메모리에서 데이터 읽기 및 확인
     printf(1, "Data from memory: %s\n", memory);
-
-    // 파일 닫기 전에 변경사항 동기화 (선택적)
-    msync(memory, size);  // msync가 xv6에 없을 경우 이 라인은 생략
 
     // 파일 닫기
     close(fd);
@@ -46,7 +47,6 @@ int main(int argc, char *argv[]) {
         exit();
     }
 
-    char buffer[100];
     int n = read(fd, buffer, sizeof(buffer));
     buffer[n] = 0; // Null-terminate the string
 
