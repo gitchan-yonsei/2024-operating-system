99c8fdeb9a14325b0137ba50f9ad833ab1ce10d4
gitchan <eunkeeeee@gmail.com>
2024-05-16 15:33:34 +0900

    feat: add proc properties struct

--- proc.h
+++ proc.h
@@ -65,6 +65,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct mmap_region mmap_regions[4];
+  int mmap_count;
 };
 
 // Process memory is laid out contiguously, low addresses first:
