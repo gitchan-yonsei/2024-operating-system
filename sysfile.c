a4b4aea41964e37cb50bb088fcd45cb2c903e7cd
gitchan <eunkeeeee@gmail.com>
2024-05-17 14:19:11 +0900

    fix: ;

--- sysfile.c
+++ sysfile.c
@@ -619,7 +619,7 @@ int munmap(void* addr, int length)
 
     // If there are no mappings in the specified address range, then you just return 0
     if (!found) {
-        panic("not found!")
+        panic("not found!");
         return 0;
     }
 
