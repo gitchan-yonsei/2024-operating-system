9b50bbd7868fd98163eb61b8342120c57233c2ed
gitchan <eunkeeeee@gmail.com>
2024-05-17 14:14:34 +0900

    feat: log not found

--- sysfile.c
+++ sysfile.c
@@ -620,7 +620,7 @@ int munmap(void* addr, int length)
 
     // If there are no mappings in the specified address range, then you just return 0
     if (!found) {
-        cprintf(1, "not found!");
+        printf("%s\n", "not found!");
         return 0;
     }
 
