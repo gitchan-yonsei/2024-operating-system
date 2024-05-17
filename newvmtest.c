2177250be5fc9e714908a3d13f03c87170d13942
gitchan <eunkeeeee@gmail.com>
2024-05-17 22:48:27 +0900

    test: 4000 -> 3000

--- newvmtest.c
+++ newvmtest.c
@@ -23,7 +23,7 @@ int main(void)
     printf(1, "%d\n", frees());
 
 	// uncomment below to test 16KB stack 
-	ptr = (int*)((int)&stack_int - 0x4000);
+	ptr = (int*)((int)&stack_int - 0x3000);
 	*ptr = 0;
 
 	printf(1, "frees before malloc: %d\n", frees());
